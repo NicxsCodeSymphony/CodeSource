7f378825b6e8a0179cdda2c29312236e0b65446d
John Nico M. Edisan <nicxsician@gmail.com>
2023-12-12 17:54:01 +0800

    Update Django.py

--- Django.py
+++ Django.py
@@ -45,6 +45,84 @@ urlpattern = [
 
 9. Create a folder to myapp and name it templates
 10. create a file base.html to templates folder
-11. Generate simple html code and this <title>{% block title %}Django App {% endBlock %}</title> <div class="container">{% block content %} {% endBlock %}</div>
-12. 
+11. Generate simple html code and this <title>{% block title %}Django App {% endblock %}</title> <div class="container">{% block content %} {% endblock %}</div>
+12. Add another HTML FILE home.html
+
+{% extends "base.html" %} {% block title %} Home Page {% endblock %}
+{% block content %}
+<p>This is the home page</p>
+{% endblock %}
+
+# RENDER THE TEMPLATES
+# GO TO views.py from myapp folder
+
+def home(request):
+    return render(request, "home.html")
+
+# ADDING DATABASES
+# GO TO models.py on myapp folder
+
+class TodoItems(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+# Go to admin.py
+
+from django.contrib import admin
+from .models import TodoItems
+# Register your models here.
+
+admin.site.register(TodoItems)
+
+# MIGRATION (Everytime we make a change to your database modules we will make a migration)
+
+13. python manage.py makemigrations
+14. python manage.py migrate
+
+# Make another view
+15. make a template todos.html
+
+{% extends "base.html" %} {% block content %}
+<h1>Todo List</h1>
+<ul>
+    {% for todo in todos %}
+    <li>
+        {{ todo.title }}: {% if todo.completed %} Completed{% else %} Not Completed{% endif %}
+    </li>
+    {% endfor %}
+</ul>
+{% endblock %}
+
+# When using a variable use double curly braces {{}}
+
+# GO TO views
+
+from django.shortcuts import render, HttpResponse
+from .models import TodoItems
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    items = TodoItems.objects.all()
+    return render(request, "todos.html", {"todos": items})
+
+# Create a URL
+
+# ADMIN PANEL
+
+13. In the terminal type python manage.py createsuperuser
+
+14. To access django dashboard, run server and add /admin
+
+
+
+
+
+
+
+
+
+
   
